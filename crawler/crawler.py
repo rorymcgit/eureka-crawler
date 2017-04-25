8adc01e11c16dca4b19193bc9d8089d952a9d961
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-04-25 20:54:06 +0100

    Assigns soup title to webpage title

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -4,8 +4,7 @@ from bs4 import BeautifulSoup
 class Crawler():
     def crawl(self, url):
         self.response_page = requests.get(url)
-        # print(self.page)
 
     def return_content(self):
-        print(type(self.response_page.text))
-        # soup = BeautifulSoup(self.response_page.text)
+        self.soup = BeautifulSoup(self.response_page.text, "html.parser")
+        self.webpage_title = self.soup.title.string
--- spec/tester.py
+++ spec/tester.py
@@ -5,14 +5,14 @@ class TestingCrawler(unittest.TestCase):
 
     def setUp(self):
         self.crawler = Crawler()
+        self.crawler.crawl("https://en.wikipedia.org/wiki") # insert our double here!
 
     def test_crawler_is_instance_of_crawler(self):
         self.assertIsInstance(self.crawler, Crawler)
 
     def test_crawl_returns_status_code(self):
-        self.crawler.crawl("https://en.wikipedia.org/wiki") # insert our double here!
         self.assertEqual(self.crawler.response_page.status_code, 200)
 
     def test_crawl_returns_content(self):
-        self.crawler.crawl("https://en.wikipedia.org/wiki")
-        self.assertIn("Cats and Dogs", self.crawler.return_content())
+        self.crawler.return_content()
+        self.assertIn("Wikipedia", self.crawler.webpage_title)
