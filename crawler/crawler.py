b522deb7d4ee30b98f9d06b1311e8d652ed54bf3
Vicky Ledsom <victorialedsom@gmail.com>
2017-04-26 11:20:56 +0100

    Replace requests with urllib to load local file for testing

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,10 +1,10 @@
-import requests
+import urllib.request
 from bs4 import BeautifulSoup
 
 class Crawler():
     def crawl(self, url):
-        self.response_page = requests.get(url)
+        self.page = urllib.request.urlopen(url).read()
 
     def return_content(self):
-        self.soup = BeautifulSoup(self.response_page.text, "html.parser")
+        self.soup = BeautifulSoup(self.page, "html.parser")
         self.webpage_title = self.soup.title.string
--- spec/tester.py
+++ spec/tester.py
@@ -5,14 +5,15 @@ class TestingCrawler(unittest.TestCase):
 
     def setUp(self):
         self.crawler = Crawler()
-        self.crawler.crawl("https://en.wikipedia.org/wiki") # insert our double here!
+        self.crawler.crawl("file:///Users/vicky/Programmes/beetlecrawler/spec/website/index.html") # insert our double here!
 
     def test_crawler_is_instance_of_crawler(self):
         self.assertIsInstance(self.crawler, Crawler)
 
-    def test_crawl_returns_status_code(self):
-        self.assertEqual(self.crawler.response_page.status_code, 200)
+    # This test wont work with the local file.
+    # def test_crawl_returns_status_code(self):
+    #     self.assertEqual(self.crawler.page.status_code, 200)
 
     def test_crawl_returns_content(self):
         self.crawler.return_content()
-        self.assertIn("Wikipedia", self.crawler.webpage_title)
+        self.assertIn("Cats and Dogs", self.crawler.webpage_title)
