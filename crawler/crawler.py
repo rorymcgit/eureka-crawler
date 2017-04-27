5b6c81099b37e8a9bc0c487452860303b86b91eb
Nicholas Leacock <nicholas@swissmail.com>
2017-04-27 22:26:50 +0100

    Studying Python by commenting various lines.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -4,18 +4,27 @@ from crawler.db_translator import Translator
 
 class Crawler():
     def __init__(self, translator = Translator()):
+        # initializes an instance of the Translator class and assigns it to 'self'
         self.translator = translator
+        # similar to Ruby's instance variables
         self.translator.set_environment()
+        # calling Translator's set_environment method, which connects with the database
 
     def crawl(self, url):
         self.url = url
+        # 'instance variable' action
         self.page = urllib.request.urlopen(url).read()
+        # calls on the urllib library to open and read a webpage
         self.translator.write_url(url)
+        # takes url from previous line, sends it to translator's write url method which then passes it on to the weburls table
 
     def return_content(self):
         self.soup = BeautifulSoup(self.page, "html.parser")
+        # setting the soup variable to a BS action.
         self.webpage_title = self.soup.title.string
+        # sets variable to title string pulled from BS??
         self.translator.write_urls_and_titles(self.url, self.webpage_title)
+        #sends url and title to translator method to then pass to database. 
 
 # crawler = Crawler()
 # crawler.crawl('https://www.webpagetest.org/')
--- crawler/db_translator.py
+++ crawler/db_translator.py
@@ -1,14 +1,20 @@
 import psycopg2
 
 class Translator():
+    #  CRUD actions with the database
     def set_environment(self, db = "dbname=beetle_crawler_development"):
         self.database = psycopg2.connect(db)
+        # Initiates a connection to the database set in the parameter
         self.database_cursor = self.database.cursor()
+        # Allows Python code to execute PostgreSQL command in a database session
 
     def write_url(self, url):
         self.database_cursor.execute("INSERT INTO weburls (weburl) VALUES (%s)",
+        # writes collected url to the weburls table in the database
                 (url,))
         self.database.commit()
+        # Commits your changes in the database
+
 
     def write_urls_and_titles(self, url, title):
         self.database_cursor.execute("INSERT INTO weburlsandtitles (weburl, title) VALUES (%s, %s)",
