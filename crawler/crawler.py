25ebe9fd3d195357cb2ac914831d330f2e23ad98
Vicky Ledsom <victorialedsom@gmail.com>
2017-04-28 19:19:18 +0100

    manually merging crawler from master. fix conflicts

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,31 +1,32 @@
 import urllib.request
 from bs4 import BeautifulSoup
-from crawler.db_translator import Translator
+from crawler.db_translator2 import Translator
 
 class Crawler():
     def __init__(self, translator = Translator()):
-        # initializes an instance of the Translator class and assigns it to 'self'
         self.translator = translator
-        # similar to Ruby's instance variables
         self.translator.set_environment()
-        # calling Translator's set_environment method, which connects with the database
 
     def crawl(self, url):
         self.url = url
-        # 'instance variable' action
         self.page = urllib.request.urlopen(url).read()
-        # calls on the urllib library to open and read a webpage
         self.translator.write_url(url)
-        # takes url from previous line, sends it to translator's write url method which then passes it on to the weburls table
 
-    def return_content(self):
+    def return_all_content(self):
         self.soup = BeautifulSoup(self.page, "html.parser")
-        # setting the soup variable to a BS action.
         self.webpage_title = self.soup.title.string
-        # sets variable to title string pulled from BS??
-        self.translator.write_urls_and_titles(self.url, self.webpage_title)
-        #sends url and title to translator method to then pass to database. 
+        self.webpage_description = self.soup.find("meta", {"name":"description"})['content']
+        self.webpage_keywords = self.soup.find("meta", {"name":"keywords"})['content']
+        self.save_found_weburls()
+        self.translator.write_urls_and_content(self.url, self.webpage_title, self.webpage_description, self.webpage_keywords)
+
+    def save_found_weburls(self):
+        self.webpage_urls = []
+        for link in self.soup.find_all('a', href=True):
+            self.webpage_urls.append(link['href'])
+        self.translator.prepare_urls_for_writing_to_db(self.webpage_urls)
+
 
 # crawler = Crawler()
 # crawler.crawl('https://www.webpagetest.org/')
-# crawler.return_content()
+# crawler.return_all_content()
