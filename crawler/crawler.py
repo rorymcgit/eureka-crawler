da574aac1fd548cc2dd1c24722667a40b2867d15
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-04-30 20:03:36 +0100

    implement crawl_next_url and guard clause

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -18,16 +18,22 @@ class Crawler():
         self.webpage_description = soup.find("meta", {"name":"description"})['content']
         self.webpage_keywords = soup.find("meta", {"name":"keywords"})['content']
         self.translator.write_urls_and_content(self.url, self.webpage_title, self.webpage_description, self.webpage_keywords)
-        next_url_to_crawl = self.translator.get_next_url()
-        while self.translator.get_weburls_table_size() < self.translator.database_limit and self.translator.get_weburls_and_content_table_size() < self.translator.database_limit:
-            self.crawl(next_url_to_crawl)
-
+        self.crawl_next_url
 
     def save_found_weburls(self, soup):
         self.webpage_urls = []
         for link in soup.find_all('a', href=True):
             self.webpage_urls.append(link['href'])
         self.translator.prepare_urls_for_writing_to_db(self.webpage_urls)
+
+    def crawl_next_url(self):
+        next_url_to_crawl = self.translator.get_next_url()
+        if self.translator.get_weburls_table_size() < self.translator.database_limit and self.translator.get_weburls_and_content_table_size() < self.translator.database_limit:
+            keep_crawling = True
+        else:
+            return 'The databases are full'
+        while keep_crawling == True:
+            self.crawl(next_url_to_crawl)
 
 # crawler = Crawler()
 # crawler.crawl('https://www.webpagetest.org/')
--- spec/crawler_tests.py
+++ spec/crawler_tests.py
@@ -47,11 +47,6 @@ class TestingCrawler(unittest.TestCase):
         self.crawler.return_all_content()
         self.translator.write_urls_and_content.assert_called_once_with(self.local_html_file, "Cats and Dogs", "Page about cats and dogs", "cats,dogs")
 
-    def test_return_all_content_calls_translator_get_next_url(self):
-        self.translator.get_next_url = MagicMock()
-        self.crawler.return_all_content()
-        self.translator.get_next_url.assert_called_once()
-
 
     def test_save_found_weburls_saves_all_urls_from_webpage_in_an_array(self):
         test_soup = BeautifulSoup('<!DOCTYPE html>\n<html>\n\n<head>\n <title>Cats and Dogs</title>\n</head><body><a href="www.dogs.com">Dogs</a><a href="www.cats.com">Cats</a></body></html>', 'html.parser')
@@ -72,7 +67,13 @@ class TestingCrawler(unittest.TestCase):
         test_title = test_soup.title.string
         self.assertNotIn(test_title, self.crawler.webpage_urls)
 
+
+    def test_crawl_next_url_calls_translator_get_next_url(self):
+        self.translator.get_next_url = MagicMock()
+        self.crawler.crawl_next_url()
+        self.translator.get_next_url.assert_called_once()
+
     def test_crawl_next_url_will_not_crawl_when_both_tables_are_full(self):
         self.translator.get_weburls_table_size = MagicMock(return_value=1000)
         self.translator.get_weburls_and_content_table_size = MagicMock(return_value=1000)
-        self.assertEqual(self.translator.crawl_next_url(), 'The databases are full')
+        self.assertEqual(self.crawler.crawl_next_url(), 'The databases are full')
